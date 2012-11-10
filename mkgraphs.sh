765ceb00890fee2992cffefa82bd77a52bd99420
Nati CT <nati@nocoot.com>
2012-11-10 17:36:54 +0200

    changing dir names to consist with table names

--- mkgraphs.sh
+++ mkgraphs.sh
@@ -20,20 +20,20 @@ popd
 
 # Generate Graphs
 ./dblp2graph.py -i $XML_DIR -o ./all &
-./dblp2graph.py -i $XML_DIR -o ./2002-2009 -e 2009-12-31 &
-./dblp2graph.py -i $XML_DIR -o ./2010-2012 -s 2010-01-01 &
+./dblp2graph.py -i $XML_DIR -o ./2002_2009 -e 2009-12-31 &
+./dblp2graph.py -i $XML_DIR -o ./2010_2012 -s 2010-01-01 &
 
 wait
 
-./dblp2graph.py -i $XML_DIR -o ./all-core3 --core 3 &
-./dblp2graph.py -i $XML_DIR -o ./2002-2009-core3 -e 2009-12-31 --core 3 &
-./dblp2graph.py -i $XML_DIR -o ./2010-2012-core3 -s 2010-01-01 --core 3 &
+./dblp2graph.py -i $XML_DIR -o ./all_core3 --core 3 &
+./dblp2graph.py -i $XML_DIR -o ./2002_2009_core3 -e 2009-12-31 --core 3 &
+./dblp2graph.py -i $XML_DIR -o ./2010_2012_core3 -s 2010-01-01 --core 3 &
 
 wait
 
 # Print stats
 printf "Set Name\t:\tNode Count\t:\tEdge Count\n"
-for s in all all-core3 2002-2009 2002-2009-core3 2010-2012 2010-2012-core3; do
+for s in all all_core3 2002_2009 2002_2009_core3 2010_2012 2010_2012_core3; do
 	printf "$s\t:\t%d\t:\t%d\n" \
 		$(wc -l ./$s/nodes.csv | cut -f1 -d' ') \
 		$(wc -l ./$s/edges.csv | cut -f1 -d' ')
