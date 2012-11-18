bbf761f8acbe17161144fb9b82ad3bb534ecdb98
Nati CT <nati@nocoot.com>
2012-11-18 09:30:04 +0200

    using intermidiate indexed tables to speed-up the benchmarks

--- benchmark-mysql.sh
+++ benchmark-mysql.sh
@@ -5,10 +5,29 @@ set -o errexit
 source ./commons.sh
 source ./graphs-conf.sh
 
+run_foo() {
+	local gn="$1" 	# graph name
+	local fn="$2"	# function name
+	local desc="$3"	# description
+	local out="$4"	# output file
+
+	t=$(timer)
+	echo -e "\t${desc} (${gn})..."
+	if [ -z "$out" ]; then
+		echo "CALL ${fn}('${gn}');" | mysql -ugdbb gdbb
+	else
+		echo "CALL ${fn}('${gn}');" | mysql -ugdbb gdbb 2>&1 >$out
+	fi
+	printf 'Elapsed time: %s\n' $(timer $t)
+}
+
 echo "Running benchmarks:"
 for g in ${GRAPH_NAMES[@]}; do
-	t=$(timer)
-	echo -e "\tCommon Neighbors ($g)..."
-	echo "CALL common_neighbors2('$g');" | mysql -ugdbb gdbb 2>&1 > ${g}/common_neighbors.out
-	printf 'Elapsed time: %s\n' $(timer $t)
+	run_foo "$g" "create_cn_table" "Creating common neighbors table"
+	run_foo "$g" "create_cnc_table" "Creating common neighbors count table"
+	run_foo "$g" "create_neighbors_table" "Creating neighbors table"
+	run_foo "$g" "b_Common_Neighbors" "Benchmarking Common Neighbors" "${g}/Common_Neighbors.out"
+	run_foo "$g" "b_Jaccard_Coefficient" "Benchmarking Jaccard's Coefficient" "${g}/Jaccard_Coefficient.out"
+	run_foo "$g" "b_Adamic_Adar" "Benchmarking Adamic/Adar" "${g}/Adamic_Adar.out"
+	run_foo "$g" "b_Preferential_attachment" "Benchmarking Preferential attachment" "${g}/Preferential_attachment.out"
 done
--- mysql/init.sql
+++ mysql/init.sql
@@ -14,7 +14,7 @@ BEGIN
  EXECUTE dropEdges;
  EXECUTE dropNodes;
  SET @createNodesStr = CONCAT('CREATE TABLE ',@nodesTable,' (
- `id` int(11) UNSIGNED NOT NULL,
+ `id` MEDIUMINT UNSIGNED NOT NULL,
  `name` varchar(64) NOT NULL,
  PRIMARY KEY (`id`)
  )');
@@ -23,8 +23,8 @@ BEGIN
  SET @fk1 = CONCAT('fk_',@edgesTable,'_id1');
  SET @fk2 = CONCAT('fk_',@edgesTable,'_id2');
  SET @createEdgesStr = CONCAT('CREATE TABLE ',@edgesTable,' (
- `id1` int(11) UNSIGNED NOT NULL,
- `id2` int(11) UNSIGNED NOT NULL,
+ `id1` MEDIUMINT UNSIGNED NOT NULL,
+ `id2` MEDIUMINT UNSIGNED NOT NULL,
  PRIMARY KEY (`id1`,`id2`),
  KEY `',@fk1,'` (`id1`),
  KEY `',@fk2,'` (`id2`),
@@ -89,4 +89,165 @@ limit 0,100;');
 PREPARE cn FROM @selectCN;
 EXECUTE cn;
 
+END $$
+
+DROP PROCEDURE IF EXISTS `create_cn_table` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `create_cn_table`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+DROP TABLE IF EXISTS `',name,'_cn`;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+CREATE TABLE ',name,'_cn (
+  `id1` mediumint(8) unsigned NOT NULL,
+  `id2` mediumint(8) unsigned NOT NULL,
+  `id3` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id1`,`id2`,`id3`),
+  KEY `fk_',name,'_pair` (`id1`, `id2`)
+)');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+INSERT INTO `',name,'_cn`
+select e1.id1 as x, e2.id1 as y, e1.id2 as n
+from ',name,'_edges as e1
+join ',name,'_edges as e2 on (e1.id2=e2.id2 and e1.id1<e2.id1);
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+DROP PROCEDURE IF EXISTS `create_cnc_table` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `create_cnc_table`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+DROP TABLE IF EXISTS `',name,'_cnc`;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+CREATE TABLE `',name,'_cnc` (
+  `id1` mediumint(8) unsigned NOT NULL,
+  `id2` mediumint(8) unsigned NOT NULL,
+  `count` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id1`,`id2`)
+)');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+INSERT INTO `',name,'_cnc`
+select id1 as x, id2 as y, count(id3) as c
+from ',name,'_cn
+group by x,y;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+DROP PROCEDURE IF EXISTS `create_neighbors_table` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `create_neighbors_table`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+DROP TABLE IF EXISTS `',name,'_neighbors`;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+CREATE TABLE `',name,'_neighbors` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `neighbors` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+)');
+PREPARE foo FROM @str;
+EXECUTE foo;
+
+SET @str = CONCAT('
+INSERT INTO `',name,'_neighbors`
+select id1 as x, count(id2) as n
+from ',name,'_edges
+group by x;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+# Common Neighbors
+DROP PROCEDURE IF EXISTS `b_Common_Neighbors` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `b_Common_Neighbors`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+SELECT
+`',name,'_cnc`.`id1`,
+`',name,'_cnc`.`id2`,
+`',name,'_cnc`.`count`
+FROM `gdbb`.`',name,'_cnc`
+order by count desc
+limit 0,100;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+# Jaccard's Coefficient
+DROP PROCEDURE IF EXISTS `b_Jaccard_Coefficient` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `b_Jaccard_Coefficient`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+select  cnc.id1 as x, 
+        cnc.id2 as y, 
+        cnc.count as i, 
+        (n1.neighbors + n2.neighbors - cnc.count) as u, 
+        cnc.count/(n1.neighbors + n2.neighbors - cnc.count) as score
+from ',name,'_cnc as cnc
+join ',name,'_neighbors as n1 on cnc.id1=n1.id
+join ',name,'_neighbors as n2 on cnc.id2=n2.id
+order by score desc
+limit 0,100;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+# Adamic/Adar
+DROP PROCEDURE IF EXISTS `b_Adamic_Adar` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `b_Adamic_Adar`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+SELECT
+`',name,'_cn`.`id1` as x,
+`',name,'_cn`.`id2` as y,
+SUM(1/(LOG10(`',name,'_neighbors`.`neighbors`))) as score
+FROM `gdbb`.`',name,'_cn`
+join `',name,'_neighbors` on (`',name,'_cn`.`id3` = `',name,'_neighbors`.`id`)
+group by x, y
+order by score desc
+limit 0,100;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
+END $$
+
+# Preferential attachment
+DROP PROCEDURE IF EXISTS `b_Preferential_attachment` $$
+CREATE DEFINER=`gdbb`@`localhost` PROCEDURE `b_Preferential_attachment`( IN name VARCHAR(16) )
+BEGIN
+SET @str = CONCAT('
+select  cnc.id1 as x, 
+        cnc.id2 as y, 
+        (n1.neighbors * n2.neighbors) as score
+from ',name,'_cnc as cnc
+join ',name,'_neighbors as n1 on cnc.id1=n1.id
+join ',name,'_neighbors as n2 on cnc.id2=n2.id
+order by score desc
+limit 0,100;
+');
+PREPARE foo FROM @str;
+EXECUTE foo;
 END $$
