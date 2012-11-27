ac5c65f7e55aacbbf3d5465d9ab327e15b785fac
Nati CT <nati@nocoot.com>
2012-11-27 06:59:24 +0200

    removing data-set-specific opertations from init script

--- init-mysql.sh
+++ init-mysql.sh
@@ -14,17 +14,3 @@ mysql -uroot -p < mysql/root.sql
 
 echo "Creating Procedures..."
 mysql -ugdbb gdbb < mysql/init.sql
-
-echo "Loading graphs:"
-for g in ${GRAPH_NAMES[@]}; do
-	echo -e "\tloading $g..."
-	t=$(timer)
-	n=$(readlink -f $g/nodes.csv)
-	e=$(readlink -f $g/edges.csv)
-	echo "CALL create_graph_tables('$g');" | mysql -ugdbb gdbb
-	echo "LOAD DATA LOCAL INFILE '$n' INTO TABLE ${g}_nodes FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';" \
-		| mysql --local-infile=1 -ugdbb gdbb
-	echo "LOAD DATA LOCAL INFILE '$e' INTO TABLE ${g}_edges FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';" \
-		| mysql --local-infile=1 -ugdbb gdbb
-	printf 'Elapsed time: %s\n' $(timer $t)
-done
