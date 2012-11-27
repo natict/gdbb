c61e66d04a3a097bc0579ff14a1714498682cfa3
Nati CT <nati@nocoot.com>
2012-11-27 06:58:19 +0200

    spliting the timer function into sub-functions

--- commons.sh
+++ commons.sh
@@ -1,19 +1,29 @@
 #! /bin/bash
 
+function tdiff() {
+	local stime=$1
+	local etime=$(date '+%s')
+
+	if [[ -z "$stime" ]]; then stime=$etime; fi
+
+	local dt=$((etime - stime))
+
+	echo $dt
+}
+
+function tprint() {
+	local dt=$1
+	ds=$((dt % 60))
+	dm=$(((dt / 60) % 60))
+	dh=$((dt / 3600))
+	printf '%d:%02d:%02d' $dh $dm $ds
+}
+
 function timer()
 {
     if [[ $# -eq 0 ]]; then
         echo $(date '+%s')
     else
-        local  stime=$1
-        etime=$(date '+%s')
-
-        if [[ -z "$stime" ]]; then stime=$etime; fi
-
-        dt=$((etime - stime))
-        ds=$((dt % 60))
-        dm=$(((dt / 60) % 60))
-        dh=$((dt / 3600))
-        printf '%d:%02d:%02d' $dh $dm $ds
+        tprint $(tdiff $1)
     fi
 }
