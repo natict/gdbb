56c9514f3d47a39540d2bb9644bede80f3195976
Nati CT <nati@nocoot.com>
2013-02-02 11:06:30 +0200

    init-mysql support password on cmd-line (insecure. tests only)

--- init-mysql.sh
+++ init-mysql.sh
@@ -6,8 +6,12 @@ echo "Creating gdbb database and user, using the following SQL:"
 echo '----------------------------------------'
 cat mysql/root.sql
 echo '----------------------------------------'
-echo "Please use mysql's root permissions."
-mysql -uroot -p < mysql/root.sql
+if [ -z "$1" ]; then
+	echo "Please use mysql's root permissions."
+	mysql -uroot -p < mysql/root.sql
+else
+	mysql -uroot -p"$1" < mysql/root.sql
+fi
 
 echo "Creating Procedures..."
 mysql -ugdbb gdbb < mysql/init.sql
