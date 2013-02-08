6af74dfa4983c570bfe0c12b43931aaf67838f0d
Nati CT <nati@nocoot.com>
2013-02-08 09:44:17 +0200

    moved table creation and population to init-mysql

--- init-mysql.sh
+++ init-mysql.sh
@@ -2,16 +2,29 @@
 
 set -o errexit
 
+# $0 <data-set-dir> [<mysql root password>]
+[ ! -d $1 ] && (echo "usage: $0 <data-set-dir>"; exit 1)
+
 echo "Creating gdbb database and user, using the following SQL:"
 echo '----------------------------------------'
 cat mysql/root.sql
 echo '----------------------------------------'
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
 	echo "Please use mysql's root permissions."
 	mysql -uroot -p < mysql/root.sql
 else
-	mysql -uroot -p"$1" < mysql/root.sql
+	mysql -uroot -p"$2" < mysql/root.sql
 fi
 
 echo "Creating Procedures..."
 mysql -ugdbb gdbb < mysql/init.sql
+
+echo "Creating Tables..."
+MYSQL_CMD="mysql --local-infile=1 -ugdbb gdbb"
+# Drop all tables
+mysqldump -ugdbb --add-drop-table --no-data gdbb | grep ^DROP | $MYSQL_CMD
+# Create nodes and edges tables
+echo "CALL create_graph_tables()" | $MYSQL_CMD
+# Load csv files
+echo "LOAD DATA LOCAL INFILE '${1}/nodes.csv' INTO TABLE nodes FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';" | $MYSQL_CMD
+echo "LOAD DATA LOCAL INFILE '${1}/edges.csv' INTO TABLE edges FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';" | $MYSQL_CMD
