ad6af33d04e933f110baaf3c149537b3aec35887
Nati CT <nati@nocoot.com>
2013-02-09 16:11:53 +0200

    added a short script to automate benchmarking over all datasets

---
+++ benchmark-all.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+set -o errexit
+
+source ./graphs-conf.sh
+
+# MySQL
+echo "Benchmarking all data-sets using MySQL:"
+sudo /etc/init.d/mysql start
+sleep 1
+for s in ${ALL_GRAPH_NAMES[@]}; do
+	echo "$s"
+	./init-mysql.sh $s $1
+	./benchmark_mysql.py > $s/mysql.json
+	sleep 1
+done
+sudo /etc/init.d/mysql stop
+sleep 1
+
+# Redis
+echo "Benchmarking all data-sets using Redis:"
+sudo redis-server /etc/redis/redis.conf
+sleep 1
+for s in ${ALL_GRAPH_NAMES[@]}; do
+	echo "$s"
+	./benchmark_redis.py -f $s/edges.csv > $s/redis.json
+	sleep 1
+done
+redis-cli shutdown
+sleep 1
+
+# Neo4J
+echo "Benchmarking all data-sets using Neo4J:"
+for s in ${ALL_GRAPH_NAMES[@]}; do
+	echo "$s"
+	./init-neo4j.sh $s
+	sleep 1
+	./benchmark_neo4j.py > $s/neo4j.json
+	sleep 1
+done
+sudo /etc/init.d/neo4j-service stop
